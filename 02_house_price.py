b49d5475f8c6824a7166c4c982aca7cabdb7b15e
Jeremy Cunningham <jpcunningh@gmail.com>
2019-04-06 11:30:26 -0500

    Removed unused split_train

--- 02_house_price.py
+++ 02_house_price.py
@@ -75,12 +75,6 @@ def test_set_check(identifier, test_ratio):
     return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
 
 
-def split_train_test_by_id(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
-
-
 rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
 
 
